dcbb7e054dff78f3c3b8087735be629ad1c48bfe
LE LOUER Lucas <lucaslelouer@gmail.com>
2023-07-22 16:37:59 +0200

    Home page done

---
+++ starter-code/script.js
@@ -0,0 +1,7 @@
+document.getElementById('hamburger-icon').addEventListener('click', () => {
+    document.querySelectorAll('.mobile-nav-container')[0].classList.toggle('is-active');
+})
+
+document.getElementById('icon-close').addEventListener('click', () => {
+    document.querySelectorAll('.mobile-nav-container')[0].classList.toggle('is-active');
+})
