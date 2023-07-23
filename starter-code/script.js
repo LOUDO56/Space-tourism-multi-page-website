d315ae93e76ba9a359d47797e71c4284043b80f1
LE LOUER Lucas <lucaslelouer@gmail.com>
2023-07-23 19:47:39 +0200

    Added animations Destination Page

--- starter-code/script.js
+++ starter-code/script.js
@@ -35,9 +35,27 @@ function changePlanetInfo(planetData){
 	const planetImg = document.querySelector('.planet-img')
 	const planetDistance = document.getElementById('planet-distance')
 	const planetTravelTime = document.getElementById('planet-travel')
+	const animations = [
+		{ element: planetName, props: [{ transform: "translate(50px)", opacity: 0 }, { transform: "translate(0)", opacity: 1 }] },
+		{ element: planetDesc, props: [{ transform: "translate(50px)", opacity: 0 }, { transform: "translate(0)", opacity: 1 }] },
+		{ element: planetTravelTime, props: [{ transform: "translateY(50px)", opacity: 0 }, { transform: "translateY(0)", opacity: 1 }] },
+		{ element: planetDistance, props: [{ transform: "translateY(50px)", opacity: 0 }, { transform: "translateY(0)", opacity: 1 }] },
+	  ];
+	let onWhatPage = 0;
 
 	for(let i = 0; i < linkPlanet.length; i++){
 		linkPlanet[i].addEventListener('click', () => {
+			if(onWhatPage !== i){
+				planetImg.animate([
+					{opacity: 0, transform: "scale(0.98)"},
+					{opacity: 1, transform: "scale(1)"}
+				], {
+					duration: 300
+				})
+				for (const animation of animations) {
+					animation.element.animate(animation.props, { duration: 115 });
+				  }
+			}
 			planetName.textContent = planetData.destinations[i].name;
 			planetDesc.textContent = planetData.destinations[i].description;
 			planetImg.src = planetData.destinations[i].images.png;
@@ -50,6 +68,7 @@ function changePlanetInfo(planetData){
 			}
 			linkPlanet[i].classList.toggle('active-hover')
 			linkPlanet[i].classList.remove('not-active')
+			onWhatPage = i;
 			
 			
 		})
